a1616fa247e9392cdcc2009f0d977f664a1ccd2e
Ahmad Alawnah <aalawneh19@gmail.com>
2021-08-03 14:57:38 +0300

    Added optional query parameters support for WeatherAPI

--- src/main/java/apis/WeatherAPI.java
+++ src/main/java/apis/WeatherAPI.java
@@ -3,7 +3,6 @@ package apis;
 import com.github.kevinsawicki.http.HttpRequest;
 import org.json.JSONObject;
 
-//TODO: overload methods to allow for optional parameters
 public class WeatherAPI {
     private String baseURL = "https://sitaopen.api.aero/weather/v1";
     private String apiKey;
@@ -39,6 +38,19 @@ public class WeatherAPI {
         return performRequest(request);
     }
 
+    public JSONObject getCurrentWeather(String airportCode){
+        return getCurrentWeather(airportCode, "K", "C");
+    }
+
+    public JSONObject getCurrentWeather(String airportCode, String lengthUnit) {
+        return getCurrentWeather(airportCode, lengthUnit, "C");
+    }
+
+    public JSONObject getCurrentWeather(String airportCode, char temperatureScale) {
+        return getCurrentWeather(airportCode, "K", ""+temperatureScale);
+    }
+
+
     public JSONObject getCurrentWeatherAndForecast(String airportCode, int forecastDuration,
                                                    String lengthUnit, String temperatureScale) {
         String url = baseURL + "/combined/" + airportCode;
@@ -53,8 +65,36 @@ public class WeatherAPI {
                 temperatureScale);
 
         return performRequest(request);
-
     }
+
+    public JSONObject getCurrentWeatherAndForecast(String airportCode) {
+        return getCurrentWeatherAndForecast(airportCode, 5, "K", "C");
+    }
+
+    public JSONObject getCurrentWeatherAndForecast(String airportCode, int forecastDuration) {
+        return getCurrentWeatherAndForecast(airportCode, forecastDuration, "K", "C");
+    }
+
+    public JSONObject getCurrentWeatherAndForecast(String airportCode, int forecastDuration, String lengthUnit) {
+        return getCurrentWeatherAndForecast(airportCode, forecastDuration, lengthUnit, "C");
+    }
+
+    public JSONObject getCurrentWeatherAndForecast(String airportCode, int forecastDuration, char temperatureScale) {
+        return getCurrentWeatherAndForecast(airportCode, forecastDuration, "K", ""+temperatureScale);
+    }
+
+    public JSONObject getCurrentWeatherAndForecast(String airportCode, String lengthUnit) {
+        return getCurrentWeatherAndForecast(airportCode, 5, lengthUnit, "C");
+    }
+
+    public JSONObject getCurrentWeatherAndForecast(String airportCode, char temperatureScale) {
+        return getCurrentWeatherAndForecast(airportCode, 5, "K", ""+temperatureScale);
+    }
+
+    public JSONObject getCurrentWeatherAndForecast(String airportCode, String lengthUnit, String temperatureScale) {
+        return getCurrentWeatherAndForecast(airportCode, 5, lengthUnit, temperatureScale);
+    }
+
 
     public JSONObject getForecast(String airportCode, int forecastDuration, String temperatureScale) {
         String url = baseURL + "/forecast/" + airportCode;
@@ -67,5 +107,17 @@ public class WeatherAPI {
                 temperatureScale);
 
         return performRequest(request);
+    }
+
+    public JSONObject getForecast(String airportCode) {
+        return getForecast(airportCode, 5, "C");
+    }
+
+    public JSONObject getForecast(String airportCode, int forecastDuration) {
+        return getForecast(airportCode, forecastDuration, "C");
+    }
+
+    public JSONObject getForecast(String airportCode, String temperatureScale) {
+        return getForecast(airportCode, 5, temperatureScale);
     }
 }
